1a32af2c2ec948995bc9c05d4bd8d282bf3f2172
Deepak DJ <dj@Deepaks-MacBook-Air.local>
2020-09-08 09:28:08 +0530

    ds

--- assets/js/serviceworker.js
+++ assets/js/serviceworker.js
@@ -4,7 +4,7 @@ if ("serviceWorker" in navigator) {
     } else {
         // Register the service worker
         navigator.serviceWorker
-            .register("{{ site.baseurl }}/serviceworker.js", {
+            .register("serviceworker.js", {
                 scope: "./"
             })
             .then(function (reg) {
